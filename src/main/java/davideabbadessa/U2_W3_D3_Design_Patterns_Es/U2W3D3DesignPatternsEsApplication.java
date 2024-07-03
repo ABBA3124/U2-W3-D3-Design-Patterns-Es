9549ca9e7df7e67f3049ab443987fcac32bd8050
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-03 14:43:38 +0200

    .

---
+++ src/main/java/davideabbadessa/U2_W3_D3_Design_Patterns_Es/U2W3D3DesignPatternsEsApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W3_D3_Design_Patterns_Es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W3D3DesignPatternsEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W3D3DesignPatternsEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/U2_W3_D3_Design_Patterns_Es/U2W3D3DesignPatternsEsApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W3_D3_Design_Patterns_Es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D3DesignPatternsEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
