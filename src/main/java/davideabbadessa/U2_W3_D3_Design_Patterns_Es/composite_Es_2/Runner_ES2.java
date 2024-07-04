c1cfeb3f5466c9c24f42b1e9ce84769d970c2ebe
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-04 15:02:15 +0200

    .

--- src/main/java/davideabbadessa/U2_W3_D3_Design_Patterns_Es/composite_Es_2/Runner_ES2.java
+++ src/main/java/davideabbadessa/U2_W3_D3_Design_Patterns_Es/composite_Es_2/Runner_ES2.java
@@ -8,28 +8,28 @@ public class Runner_ES2 implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // <<-------------------------------ES2------------------------------->>
-        System.out.println("<<-------------------------------ES2------------------------------->>");
-        Libro libro = new Libro();
-        libro.aggiungiAutore("Davide Abbadessa");
-        libro.aggiungiAutore("Alessio Vulpinari");
-        libro.aggiungiAutore("Cristina Martucci");
-        libro.setPrezzo(99.99);
-
-        Pagina pagina1 = new Pagina(1);
-        Pagina pagina2 = new Pagina(2);
-
-        Sezione sezione1 = new Sezione("Sezione 1 Il principe");
-        sezione1.aggiungi(pagina1);
-
-        Sezione sezione2 = new Sezione("Sezione 2 Il full stack");
-        sezione2.aggiungi(pagina2);
-        sezione2.aggiungi(sezione1);
-
-        libro.aggiungiComponente(sezione2);
-
-        libro.stampa();
-        System.out.println("Numero totale di pagine: " + libro.getNumeroPagine());
-        System.out.println("Prezzo del libro: " + libro.getPrezzo());
+//        System.out.println("<<-------------------------------ES2------------------------------->>");
+//        Libro libro = new Libro();
+//        libro.aggiungiAutore("Davide Abbadessa");
+//        libro.aggiungiAutore("Alessio Vulpinari");
+//        libro.aggiungiAutore("Cristina Martucci");
+//        libro.setPrezzo(99.99);
+//
+//        Pagina pagina1 = new Pagina(1);
+//        Pagina pagina2 = new Pagina(2);
+//
+//        Sezione sezione1 = new Sezione("Sezione 1 Il principe");
+//        sezione1.aggiungi(pagina1);
+//
+//        Sezione sezione2 = new Sezione("Sezione 2 Il full stack");
+//        sezione2.aggiungi(pagina2);
+//        sezione2.aggiungi(sezione1);
+//
+//        libro.aggiungiComponente(sezione2);
+//
+//        libro.stampa();
+//        System.out.println("Numero totale di pagine: " + libro.getNumeroPagine());
+//        System.out.println("Prezzo del libro: " + libro.getPrezzo());
 
     }
 }
